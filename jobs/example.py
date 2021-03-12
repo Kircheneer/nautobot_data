689362af1b09f2befbd759790ca1c24ecd653daa
Leo Kirchner <leo@kirchne.red>
2021-03-12 17:08:46 +0100

    Daosind

--- jobs/example.py
+++ jobs/example.py
@@ -1,46 +1,70 @@
-from nautobot.dcim.choices import DeviceStatusChoices
-from nautobot.dcim.models import ConsolePort, Device, PowerPort
-from nautobot.extras.jobs import Job
+from django.utils.text import slugify
+
+from nautobot.dcim.choices import DeviceStatusChoices, SiteStatusChoices
+from nautobot.dcim.models import Device, DeviceRole, DeviceType, Manufacturer, Site
+from nautobot.extras.jobs import *
 
 
-class DeviceConnectionsReport(Job):
-    description = "Validate the minimum physical connections for each device"
+class NewBranch(Job):
 
-    def test_console_connection(self):
+    class Meta:
+        name = "New Branch"
+        description = "Provision a new branch site"
+        field_order = ['site_name', 'switch_count', 'switch_model']
 
-        # Check that every console port for every active device has a connection defined.
-        active = DeviceStatusChoices.STATUS_ACTIVE
-        for console_port in ConsolePort.objects.prefetch_related('device').filter(device__status=active):
-            if console_port.connected_endpoint is None:
-                self.log_failure(
-                    obj=console_port.device,
-                    message="No console connection defined for {}".format(console_port.name)
-                )
-            elif not console_port.connection_status:
-                self.log_warning(
-                    obj=console_port.device,
-                    message="Console connection for {} marked as planned".format(console_port.name)
-                )
-            else:
-                self.log_success(obj=console_port.device)
+    site_name = StringVar(
+        description="Name of the new site"
+    )
+    switch_count = IntegerVar(
+        description="Number of access switches to create"
+    )
+    manufacturer = ObjectVar(
+        model=Manufacturer,
+        required=False
+    )
+    switch_model = ObjectVar(
+        description="Access switch model",
+        model=DeviceType,
+        display_field='model',
+        query_params={
+            'manufacturer_id': '$manufacturer'
+        }
+    )
 
-    def test_power_connections(self):
+    def run(self, data, commit):
 
-        # Check that every active device has at least two connected power supplies.
-        for device in Device.objects.filter(status=DeviceStatusChoices.STATUS_ACTIVE):
-            connected_ports = 0
-            for power_port in PowerPort.objects.filter(device=device):
-                if power_port.connected_endpoint is not None:
-                    connected_ports += 1
-                    if not power_port.connection_status:
-                        self.log_warning(
-                            obj=device,
-                            message="Power connection for {} marked as planned".format(power_port.name)
-                        )
-            if connected_ports < 2:
-                self.log_failure(
-                    obj=device,
-                    message="{} connected power supplies found (2 needed)".format(connected_ports)
-                )
-            else:
-                self.log_success(obj=device)
+        # Create the new site
+        site = Site(
+            name=data['site_name'],
+            slug=slugify(data['site_name']),
+            status=SiteStatusChoices.STATUS_PLANNED
+        )
+        site.validated_save()
+        self.log_success(obj=site, message="Created new site")
+
+        # Create access switches
+        switch_role = DeviceRole.objects.get(name='Access Switch')
+        for i in range(1, data['switch_count'] + 1):
+            switch = Device(
+                device_type=data['switch_model'],
+                name=f'{site.slug}-switch{i}',
+                site=site,
+                status=DeviceStatusChoices.STATUS_PLANNED,
+                device_role=switch_role
+            )
+            switch.validated_save()
+            self.log_success(obj=switch, message="Created new switch")
+
+        # Generate a CSV table of new devices
+        output = [
+            'name,make,model'
+        ]
+        for switch in Device.objects.filter(site=site):
+            attrs = [
+                switch.name,
+                switch.device_type.manufacturer.name,
+                switch.device_type.model
+            ]
+            output.append(','.join(attrs))
+
+        return '\n'.join(output)
